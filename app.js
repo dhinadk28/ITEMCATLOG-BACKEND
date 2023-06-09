0ecd9e9a6ea26b030d1ecd0a5bd20c596b71a3a4
dhinadk28 <103361324+dhinadk28@users.noreply.github.com>
2023-06-09 10:08:32 +0530

    add

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'https://catlog-frontend.vercel.app',
+  origin: 'http://localhost:3000',
   credentials: true
 }));
 
